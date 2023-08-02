b865cfbd4fc041f04db7822b42639680e3f56943
ibaraki333 <141193401+ibaraki333@users.noreply.github.com>
2023-08-02 07:33:57 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const toggleIcon = document.querySelector('.toggle-icon');
+
+toggleIcon.addEventListener('click',() => {
+	toggleIcon.classList.toggle('bx-sun');
+	document.body.classList.toggle('dark-mode');
+});
+
+window.onscroll = () => {
+let header = document.querySelector('.header');
+
+header.classList.toggle('sticky', window.scrollY > 100);
+};
